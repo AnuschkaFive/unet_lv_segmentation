9f11d5adc25aeeb890230f56ecd57f4dde29c9c5
Anna Franziska Horne <afhorne@web.de>
2019-01-10 20:58:35 +0100

    training for model A set up

--- model/net.py
+++ model/net.py
@@ -6,6 +6,7 @@ Based on code by Marco Pavlowski (and https://tuatini.me/practical-image-segment
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision.models as models
 
 
 class ConvBnRelu(nn.Module):
@@ -69,6 +70,8 @@ class StackDecoder(nn.Module):
     def forward(self, x, down_tensor):
         #x = self.upSample(x)
         x = F.interpolate(x, self.upsample_size)
+        if not self.use_crop_concat:
+            x = x.add(down_tensor)
         x = self.convr1(x)
         if self.use_crop_concat:
             x = self._crop_concat(x, down_tensor)
@@ -100,6 +103,61 @@ class UNetOriginal(nn.Module):
 #        self.up2 = StackDecoder(in_channels=512, out_channels=256, kernel_size=kSize, padding=pad, stride=stride, upsample_size=(104, 104))
 #        self.up3 = StackDecoder(in_channels=256, out_channels=128, kernel_size=kSize, padding=pad, stride=stride, upsample_size=(200, 200))
 #        self.up4 = StackDecoder(in_channels=128, out_channels=64, kernel_size=kSize, padding=pad, stride=stride, upsample_size=(392, 392))
+        self.up1 = StackDecoder(in_channels=1024, out_channels=512, kernel_size=kSize, padding=pad, stride=stride, upsample_size=(40, 40), hyper_params=hyper_params)
+        self.up2 = StackDecoder(in_channels=512, out_channels=256, kernel_size=kSize, padding=pad, stride=stride, upsample_size=(80, 80), hyper_params=hyper_params)
+        self.up3 = StackDecoder(in_channels=256, out_channels=128, kernel_size=kSize, padding=pad, stride=stride, upsample_size=(160, 160), hyper_params=hyper_params)
+        self.up4 = StackDecoder(in_channels=128, out_channels=64, kernel_size=kSize, padding=pad, stride=stride, upsample_size=(320, 320), hyper_params=hyper_params)
+        # 1x1 convolution at the last layer
+        # Different from the paper is the output size here
+        self.output_seg_map = nn.Conv2d(64, 1, kernel_size=(1, 1), padding=0, stride=1)
+
+    def forward(self, x):
+        x, x_trace1 = self.down1(x)  # Calls the forward() method of each layer
+        x, x_trace2 = self.down2(x)
+        x, x_trace3 = self.down3(x)
+        x, x_trace4 = self.down4(x)
+
+        x = self.center(x)
+
+        x = self.up1(x, x_trace4)
+        x = self.up2(x, x_trace3)
+        x = self.up3(x, x_trace2)
+        x = self.up4(x, x_trace1)
+
+        out = self.output_seg_map(x)
+        #out = torch.squeeze(out, dim=1)
+        return out
+
+class UNetOriginalPretrained(nn.Module):
+    def __init__(self, hyper_params):
+        super(UNetOriginalPretrained, self).__init__()
+        # TODO: works for input image sizes 360x360 (thanks Marco!)
+        kSize = (3,3)
+        pad = 1  # padding 1 so ksize 3 doesn't shorten the output
+        stride = 1
+        pool_kSize = (2,2)
+        pool_stride = 2
+        
+        encoder = models.vgg11(pretrained=True).features
+
+        self.down1 = StackEncoder(1, 64, kernel_size=kSize, padding=pad, stride=stride, pool_kernel_size=pool_kSize, pool_stride=pool_stride, hyper_params=hyper_params)
+        self.down1.convr1.conv.weight = torch.nn.Parameter(torch.narrow(torch.tensor(encoder[0].weight), 1, 0, 1))
+        self.down1.convr1.conv.bias = encoder[0].bias
+        self.down2 = StackEncoder(64, 128, kernel_size=kSize, padding=pad, stride=stride, pool_kernel_size=pool_kSize, pool_stride=pool_stride, hyper_params=hyper_params)
+        self.down2.convr1.conv.weight = encoder[3].weight
+        self.down2.convr1.conv.bias = encoder[3].bias
+        self.down3 = StackEncoder(128, 256, kernel_size=kSize, padding=pad, stride=stride, pool_kernel_size=pool_kSize, pool_stride=pool_stride, hyper_params=hyper_params)
+        self.down3.convr1.conv.weight = encoder[6].weight
+        self.down3.convr1.conv.bias = encoder[6].bias        
+        self.down4 = StackEncoder(256, 512, kernel_size=kSize, padding=pad, stride=stride, pool_kernel_size=pool_kSize, pool_stride=pool_stride, hyper_params=hyper_params)
+        self.down4.convr1.conv.weight = encoder[11].weight
+        self.down4.convr1.conv.bias = encoder[11].bias 
+        
+        self.center = nn.Sequential(
+            ConvBnRelu(512, 1024, kernel_size=(3, 3), stride=1, padding=0, hyper_params=hyper_params),
+            ConvBnRelu(1024, 1024, kernel_size=(3, 3), stride=1, padding=0, hyper_params=hyper_params)
+        )
+
         self.up1 = StackDecoder(in_channels=1024, out_channels=512, kernel_size=kSize, padding=pad, stride=stride, upsample_size=(40, 40), hyper_params=hyper_params)
         self.up2 = StackDecoder(in_channels=512, out_channels=256, kernel_size=kSize, padding=pad, stride=stride, upsample_size=(80, 80), hyper_params=hyper_params)
         self.up3 = StackDecoder(in_channels=256, out_channels=128, kernel_size=kSize, padding=pad, stride=stride, upsample_size=(160, 160), hyper_params=hyper_params)
@@ -170,6 +228,18 @@ class ModelA(nn.Module):
         out = self.output_seg_map(x)
         #out = torch.squeeze(out, dim=1)
         return out
+
+
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    #nameclass = m.__class__
+    print(classname)
+    if classname.find('Conv2d') != -1:
+        m.weight.data.fill_(0.0)
+#    elif classname.find('BatchNorm') != -1:
+#        m.weight.data.normal_(1.0, 0.02)
+#        m.bias.data.fill_(0)
 
 #class Net(nn.Module):
 #    """
