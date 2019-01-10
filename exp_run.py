f6f2b2b265a01f398df82eaf4f361effe6ef2c1f
Anna Franziska Horne <afhorne@web.de>
2019-01-10 23:17:54 +0100

    new runs set up

--- exp_run.py
+++ exp_run.py
@@ -1,6 +1,7 @@
 import search_hyperparams
 
 if __name__ == '__main__':
-    search_hyperparams.main(parent_dir="experiments/ENDO/model_UNetOriginal_random/learning_rate/loss_cross_entropy_loss", data_dir="data/320x320_heart_scans")
-    search_hyperparams.main(parent_dir="experiments/ENDO/model_UNetOriginal_random/learning_rate/loss_soft_dice_loss", data_dir="data/320x320_heart_scans")
-    search_hyperparams.main(parent_dir="experiments/ENDO/model_UNetOriginal_random/learning_rate/loss_cross_entropy_loss/learning_rate_0.0001/dropout", data_dir="data/320x320_heart_scans")
+    search_hyperparams.main(parent_dir="experiments/ENDO/model_ModelA_random/learning_rate/loss_soft_dice_loss", 
+                            data_dir="data/320x320_heart_scans")
+    search_hyperparams.main(parent_dir="experiments/EPI/model_ModelA_random/learning_rate/loss_soft_dice_loss", 
+                            data_dir="data/320x320_heart_scans")
