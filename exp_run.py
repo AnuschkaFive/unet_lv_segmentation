7bd43b4a92f2839a69613941b1963b89c6a40fcc
Anna Franziska Horne <afhorne@web.de>
2019-01-12 14:13:23 +0100

    temp changes

--- exp_run.py
+++ exp_run.py
@@ -1,7 +1,5 @@
 import search_hyperparams
 
 if __name__ == '__main__':
-    search_hyperparams.main(parent_dir="experiments/ENDO/model_ModelA_random/learning_rate/loss_soft_dice_loss", 
-                            data_dir="data/320x320_heart_scans")
-    search_hyperparams.main(parent_dir="experiments/EPI/model_ModelA_random/learning_rate/loss_soft_dice_loss", 
+    search_hyperparams.main(parent_dir="experiments/ENDO/model_ModelA_random/treshold", 
                             data_dir="data/320x320_heart_scans")
