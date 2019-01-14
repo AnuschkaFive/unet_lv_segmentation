0d270a4310481e325dc1a3265005af9121008707
Anna Franziska Horne <afhorne@web.de>
2019-01-14 21:26:30 +0100

    synthesized table now shows more useful data

--- synthesize_results.py
+++ synthesize_results.py
@@ -26,6 +26,55 @@ def aggregate_metrics(parent_dir, metrics):
     if metrics_file.is_file():
         with open(metrics_file, 'r') as f:
             metrics[parent_dir] = json.load(f)
+            metrics[parent_dir].pop('recall', None)
+            metrics[parent_dir].pop('precision', None)
+            metrics[parent_dir].pop('accuracy', None)
+            best_epoch = ""
+            with open(Path(parent_dir) / "metrics_k_fold_val.json") as j:
+                val_metrics = json.load(j)
+                val_metrics = val_metrics['average']
+                for epochs in val_metrics:
+                    if val_metrics[epochs]['dsc'] == metrics[parent_dir]['dsc']:
+                        best_epoch = epochs
+            if best_epoch is not "":
+                metrics[parent_dir]["val"] = best_epoch
+                with open(Path(parent_dir) / "metrics_k_fold_train.json") as j:
+                    train_metrics = json.load(j)
+                    metrics[parent_dir]["dsc train"] = train_metrics["average"][best_epoch]["dsc"] - metrics[parent_dir]["dsc"]
+                    metrics[parent_dir]["loss train"] = train_metrics["average"][best_epoch]["loss"] - metrics[parent_dir]["loss"]
+                    metrics[parent_dir]["iou train"] = train_metrics["average"][best_epoch]["iou"] - metrics[parent_dir]["iou"]
+            else:
+                metrics[parent_dir]["dsc train"] = 0.0
+                metrics[parent_dir]["loss train"] = 0.0
+                metrics[parent_dir]["iou train"] = 0.0
+                metrics[parent_dir]["val"] = ""
+            test_file = Path(parent_dir) / "metrics_test.json"
+            metrics[parent_dir]["dsc test"] = 0.0
+            metrics[parent_dir]["loss test"] = 0.0
+            metrics[parent_dir]["iou test"] = 0.0
+            metrics[parent_dir]["test"] = ""
+            metrics[parent_dir]["dsc test train"] = 0.0
+            metrics[parent_dir]["loss test train"] = 0.0
+            metrics[parent_dir]["iou test train"] = 0.0
+            if test_file.is_file():
+                best_test_epoch = ""
+                with open(test_file, 'r') as t:
+                    test_metrics = json.load(t)
+                    best_dsc = 0.0
+                    for epochs in test_metrics:
+                        if test_metrics[epochs]["dsc"] > best_dsc:
+                            best_dsc = test_metrics[epochs]["dsc"]
+                            best_test_epoch = epochs
+                    if best_test_epoch is not "":
+                        metrics[parent_dir]["dsc test"] = test_metrics[best_test_epoch]["dsc"]
+                        metrics[parent_dir]["loss test"] = test_metrics[best_test_epoch]["loss"]
+                        metrics[parent_dir]["iou test"] = test_metrics[best_test_epoch]["iou"]
+                        metrics[parent_dir]["test"] = best_test_epoch
+                        with open(Path(parent_dir) / "metrics_train.json") as k:
+                            test_train_metrics = json.load(k)
+                            metrics[parent_dir]["dsc test train"] = test_train_metrics[best_test_epoch]["dsc"] - test_metrics[best_test_epoch]["dsc"]
+                            metrics[parent_dir]["loss test train"] = test_train_metrics[best_test_epoch]["loss"] - test_metrics[best_test_epoch]["loss"]
+                            metrics[parent_dir]["iou test train"] = test_train_metrics[best_test_epoch]["iou"] - test_metrics[best_test_epoch]["iou"]
 
     # Check every subdirectory of parent_dir
     for subdir in Path(parent_dir).iterdir():
